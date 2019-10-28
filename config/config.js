f654d04ad96cce0631394e62ba098bd22daf65c7
Jamie Luong <jamieluong03@gmail.com>
2019-10-28 11:54:18 -0700

    updated and cleaned the file directories

--- config/config.js
+++ config/config.js
@@ -6,7 +6,7 @@ let configs = {
   "development": {
     "username": "root",
     "password": `${process.env.DB_PASSWORD}`,
-    "database": "passport_demo",
+    "database": "recipe_db",
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
