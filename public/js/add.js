1b1fc399e9defbbce51d71319ffcc4953507fbd0
Walter Robles <robleswalter1968@gmail.com>
2019-11-01 10:29:18 -0700

    ongoing changes

--- public/js/add.js
+++ public/js/add.js
@@ -1,13 +1,46 @@
+console.log("linked");
+// Global Variables
 var counter = 1;
-var limit = 10;
-function addInput(divName){
-     if (counter == limit)  {
-          alert("You have reached the limit of adding " + counter + " inputs");
-     }
-     else {
-          var newdiv = document.createElement('div');
-          newdiv.innerHTML = "Ingredient " + (counter + 1) + " <br><input type='text' name='myInputs[]'>";
-          document.getElementById(divName).appendChild(newdiv);
-          counter++;
-     }
+var limit = 15;
+
+// Function Definitions
+function addInput(){
+    if (counter == limit)  {
+         alert("You have reached the limit of adding " + counter + " inputs");
+    } else {
+         var newdiv = document.createElement('div');
+         newdiv.setAttribute("class", "control");
+         newdiv.setAttribute("data-id", counter);
+         newdiv.innerHTML = `<input class="input ingredient" type="text" placeholder="One Ingredient Per Line">`;
+        //  newdiv.innerHTML = "Entry " + (counter + 1) + " <br><input type='text' name='myInputs[]'>";
+         document.getElementById('ingredients').appendChild(newdiv);
+         counter++;
+    }
 }
+// save new recipe
+
+function saveRecipe() {
+    // function will post customers recipe in our database
+    // gather form inputs
+    var recipeName = document.getElementById("recipeName").value;
+    console.log(recipeName);
+    
+
+    // package up inputs into an object new recipe
+    // then we post it to a server
+}
+// Methods & Function Cals
+document.addEventListener("DOMContentLoaded", function() {
+
+    document.addEventListener("click", event => {
+        if ( event.target.getAttribute("id") === "dynamicInput") {
+            addInput();
+        }
+
+        if ( event.target.getAttribute("id") === "submit") {
+            saveRecipe();
+        }
+    })
+
+
+});
